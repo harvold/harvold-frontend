6179fb0ef8f59488e76fa3f985b2d6071d999273
Puddinglax <37007834+Puddinglax@users.noreply.github.com>
2018-07-04 21:57:10 -0400

    Add files via upload

--- front-end-resources/pages/Map/Gameplay/index.js
+++ front-end-resources/pages/Map/Gameplay/index.js
@@ -54,3 +54,23 @@ function drawMap(mapArray){
         }
     }
 }
+
+// this stuff makes it not scroll
+/*
+var keys = {};
+window.addEventListener("keydown",
+    function(e){
+        keys[e.keyCode] = true;
+        switch(e.keyCode){
+            case 37: case 39: case 38:  case 40: // Arrow keys
+            case 32: e.preventDefault(); break; // Space
+            default: break; // do not block other keys
+        }
+    },
+false);
+window.addEventListener('keyup',
+    function(e){
+        keys[e.keyCode] = false;
+    },
+false);
+*/
