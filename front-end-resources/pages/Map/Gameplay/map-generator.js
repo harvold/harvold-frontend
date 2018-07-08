018231af66024da1f399ab95b756bea392fd0a3f
brucejh99 <bjhong@yahoo.com>
2018-07-07 20:43:38 -0400

    Movement logic created, todo: movement animation and implementation

--- front-end-resources/pages/Map/Gameplay/map-generator.js
+++ front-end-resources/pages/Map/Gameplay/map-generator.js
@@ -36,11 +36,19 @@ var mapArray = [
     0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
 ]
 
+var keysDown = {
+	37 : false,
+	38 : false,
+	39 : false,
+	40 : false
+};
+
 $(() => {
 	drawMap(mapArray);
 	ctx.font = "bold 10pt sans-serif";
     var player = new Character(0, 0, '#ff0800');
     drawPlayer(player);
+    // movementHandler(player);
 
 	window.addEventListener("keydown", function(e) {
 		if(e.keyCode >= 37 && e.keyCode <= 40) { 
@@ -55,6 +63,7 @@ $(() => {
 	});
 });
 
+
 function drawMap(mapArray) {
     for(var i = 0; i < height * width; i++) {
         if(mapArray[i] === 1) {
@@ -67,30 +76,75 @@ function drawMap(mapArray) {
     }
 }
 
+
 function drawPlayer(player) {
-    ctx.fillStyle = player.colour;
-    ctx.fillRect(player.position[0], player.position[1], player.height, player.width);
+    ctx.fillStyle = player.sprite;
+    ctx.fillRect(player.positionX, player.positionY, player.height, player.width);
     return player;
 }
 
-var keysDown = {
-	37 : false,
-	38 : false,
-	39 : false,
-	40 : false
-};
 
-// code for player generation
-
-// player object for testing purposes
+// player object for testing purposes. Information should be queried from database
 
 function Character(x, y, colour) {
-	this.tileFrom	= [16, 16];
-	this.tileTo		= [16, 16];
-	this.timeMoved	= 0;
 	this.width     	= 14;
     this.height     = 14;
-	this.position	= [x * 16 + 1, y * 16 + 1];
-	this.delayMove	= 100;
-    this.colour     = colour;
+    this.x          = x;
+    this.y          = y;
+	this.positionX	= x * square + 1;
+    this.positionY  = y * square + 1;
+    this.sprite     = colour;
+    this.moveCd     = 25;
+    
+    this.drawMovement = (moveValue) => {
+        // TODO NEXT: movement animation here
+    }
+    this.moveRight = () => {
+        if(x === width - 1) {
+            // generate new map here. Data should be pulled from backend.
+        } else if(mapArray[this.y * width + this.x + 1] === 0) {
+            // handle collision here. Bump back animation or walk on spot animation.
+        } else {
+            // handle valid movement
+            this.x++;
+            this.positionX = this.x * square + 1;
+            this.drawMovement(1);
+        }
+    }
+    this.moveLeft = () => {
+        if(x === 0) {
+            // generate new map here. Data should be pulled from backend.
+        } else if(mapArray[this.y * width + this.x - 1] === 0) {
+            // handle collision here. Bump back animation or walk on spot animation.
+        } else {
+            // handle valid movement
+            this.x--;
+            this.positionX = this.x * square + 1;
+            this.drawMovement(-1);
+        }
+    }
+    this.moveUp = () => {
+        if(y === 0) {
+            // generate new map here. Data should be pulled from backend.
+        } else if(mapArray[this.y * width + this.x - width] === 0) {
+            // handle collision here. Bump back animation or walk on spot animation.
+        } else {
+            // handle valid movement
+            this.y--;
+            this.positionY = this.y * square + 1;
+            this.drawMovement(-width);
+        }
+    }
+    this.moveDown = () => {
+        if(y === height - 1) {
+            // generate new map here. Data should be pulled from backend.
+        } else if(mapArray[this.y * width + this.x + width] === 0) {
+            // handle collision here. Bump back animation or walk on spot animation.
+        } else {
+            // handle valid movement
+            this.y++;
+            this.positionY = this.y * square + 1;
+            this.drawMovement(width);
+        }
+    }
 }
